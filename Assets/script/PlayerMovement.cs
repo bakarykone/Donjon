9bb2abd7e1e0e911b6f8102bf0d8a09f8c05b9d4
bakarykone <bakarykone729@gmail.com>
2024-02-28 15:46:56 +0100

    first commit

---
+++ Assets/script/PlayerMovement.cs
@@ -0,0 +1,79 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class player : MonoBehaviour
+{
+    public float speed;
+    /*
+    public InputActionReference forwardAction;
+    public InputActionReference rightAction;
+    public InputActionReference leftAction;
+    public InputActionReference downAction;
+    */
+    public InputActionReference fireAction;
+    public InputActionReference horizontalAction;
+    public InputActionReference verticalAction;
+    public GameObject bulletPrefab;
+    public GameObject bulletSpawnPoint;
+    private Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody>();
+    }
+
+    void RotateToward(Vector3 pos)
+    {
+        pos = new Vector3(pos.x, this.transform.position.y, pos.z);
+        this.transform.LookAt(pos);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        Debug.DrawRay(ray.origin, ray.direction * 100f, Color.red);
+
+        if ((Physics.Raycast(ray, out hit, 100f, 1 << 3)))
+        {
+            print("Ray hit at : " + hit.point);
+            RotateToward(hit.point);
+        }
+
+        Vector3 movement = this.transform.position;
+        //Ajout du mouvement horizontal 
+        movement += Vector3.right * speed * horizontalAction.action.ReadValue<float>() * Time.deltaTime;
+
+        //Ajout du mouvement vertical 
+        movement += Vector3.forward * speed * verticalAction.action.ReadValue<float>() * Time.deltaTime;
+
+        rb.MovePosition(movement);
+        //this.transform.position += Vector3.right * speed * horizontalAction.action.ReadValue<float>() * Time.deltaTime;
+
+
+        if (fireAction.action.triggered)
+        {
+            Instantiate(bulletPrefab, bulletSpawnPoint.transform.position, this.transform.rotation);
+        }
+        /*
+        if(downAction.action.IsInProgress())
+        {
+            this.transform.position += Vector3.back * speed * Time.deltaTime;
+        }
+        if (leftAction.action.IsInProgress())
+        {
+            this.transform.position += Vector3.left * speed * Time.deltaTime;
+        }
+        if (rightAction.action.IsInProgress())
+        {
+            this.transform.position += Vector3.right * speed * Time.deltaTime;
+        }
+        */
+    }
+}
---
+++ Assets/script/bullet.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bullet : MonoBehaviour
+{
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.position += this.transform.forward * speed * Time.deltaTime;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Wall")
+        {
+            Destroy(this.gameObject);//detruire la balle
+
+        }
+        if (collision.gameObject.tag == "Foe")
+        {
+            Destroy(collision.gameObject);// detruit l'ennemie
+            Destroy(this.gameObject);//detruire la balle
+        }
+    }
+}
