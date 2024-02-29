92914f011d664c1c4a27eaf7913cdd5f0b674a09
bakarykone <bakarykone729@gmail.com>
2024-02-29 13:00:01 +0100

    player animation done

--- Assets/script/animationStateController.cs
+++ Assets/script/animationStateController.cs
@@ -28,7 +28,10 @@ public class animationStateController : MonoBehaviour
     void Update()
     {
         bool isRunning = animator.GetBool(isRunningHash);
-        bool isWalking = animator.GetBool(isWalkingForwardHash);
+        bool isWalkingForward = animator.GetBool(isWalkingForwardHash);
+        bool isWalkingLeft = animator.GetBool(isWalkingLeftHash);
+        bool isWalkingRight = animator.GetBool(isWalkingRightHash);
+        bool isWalkingBackward = animator.GetBool(isWalkingBackwardHash);
         bool forwardPressed = Input.GetKey("w");
         bool runPressed = Input.GetKey("left shift");
         bool leftPressed = Input.GetKey("a");
@@ -36,14 +39,14 @@ public class animationStateController : MonoBehaviour
         bool backwardPressed = Input.GetKey("s");
 
     //FORWARD
-        //if player presses w key
-        if (!isWalking && forwardPressed) 
+        //if player presses W key
+        if (!isWalkingForward && forwardPressed) 
         {
             //then set the walking boolean to be true
             animator.SetBool(isWalkingForwardHash, true);
         }
-        // if player is not pressing w key
-        if (isWalking && !forwardPressed)
+        // if player is not pressing W key
+        if (isWalkingForward && !forwardPressed)
         {
             //then set the isWalking boolean to be false
             animator.SetBool(isWalkingForwardHash, false);
@@ -62,18 +65,46 @@ public class animationStateController : MonoBehaviour
             animator.SetBool(isRunningHash, false);
         }
 
-        //LEFT
-        //if player presses a key
-        if (!isWalking && leftPressed)
+    //LEFT
+        //if player presses A key
+        if (!isWalkingLeft && leftPressed)
         {
             //then set the walking boolean to be true
             animator.SetBool(isWalkingLeftHash, true);
         }
-        // if player is not pressing a key
-        if (isWalking && !forwardPressed)
+        // if player is not pressing A key
+        if (isWalkingLeft && !leftPressed)
         {
             //then set the isWalking boolean to be false
             animator.SetBool(isWalkingLeftHash, false);
         }
+
+    //RIGHT
+        //if player presses D key
+        if (!isWalkingRight && rightPressed)
+        {
+            //then set the walking boolean to be true
+            animator.SetBool(isWalkingRightHash, true);
+        }
+        // if player is not pressing D key
+        if (isWalkingRight && !rightPressed)
+        {
+            //then set the isWalking boolean to be false
+            animator.SetBool(isWalkingRightHash, false);
+        }
+
+    //BACKWARD
+        if (!isWalkingBackward && backwardPressed)
+        {
+            //then set the walking boolean to be true
+            animator.SetBool(isWalkingBackwardHash, true);
+        }
+        // if player is not pressing W key
+        if (isWalkingBackward && !forwardPressed)
+        {
+            //then set the isWalking boolean to be false
+            animator.SetBool(isWalkingBackwardHash, false);
+        }
+
     }
 }
