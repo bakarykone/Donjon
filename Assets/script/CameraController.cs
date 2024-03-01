8dc4b2ba44bbdd56084ee76d9692bc6a8b83c3f3
bakarykone <bakarykone729@gmail.com>
2024-03-01 15:38:03 +0100

    IA added and camera controller fix

--- Assets/script/CameraController.cs
+++ Assets/script/CameraController.cs
@@ -5,15 +5,18 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     public GameObject player;
+    private Vector3 offset;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        offset = this.transform.position - player.transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        this.transform.position = new Vector3(player.transform.position.x, transform.position.y, player.transform.position.z);
+        this.transform.position = player.transform.position + offset;
+        //this.transform.position = new Vector3(player.transform.position.x, this.transform.position.y, player.transform.position.z); 
     }
 }
